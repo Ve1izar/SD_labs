21f3fccf36e3658e262fef57ee7637c0c8ba22cd
Горечко Владислав <brianprott456@gmail.com>
2026-03-01 15:15:59 +0200

    Initial commit: Hello World

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    name = input("Enter your name: ")
+    if name.strip() == "":
+        print("Рядок не може бути пустим")
+    else:
+        print(f"Hello, {name}!")
+        break
